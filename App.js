4da111fc15dfa863d5b15205b2d75f58801bfb64
Countryreid <117122319+Countryreid@users.noreply.github.com>
2023-06-17 21:52:40 -0600

    Functioning inputs

--- App.js
+++ App.js
@@ -14,89 +14,71 @@ export default function App() {
   const [averageHorseWeight, setaverageHorseWeight] = useState('');
   const [fieldDensity, setfieldDensity] = useState('');
 
-  const [text, setText] = useState('');
 
+  const feedDays = ((pastureSize * (pastureHeight * 200) * 0.5  )/(horseCount * averageHorseWeight * percentBodyWeight))
 
-  var feedDays = ((pastureSize * (pastureHeight * 200) * 0.5  )/(horseCount * averageHorseWeight * percentBodyWeight))
+  const tonsDryFeed = pastureSize*pastureHeight*grassMix
+
 
   return (
-    <View style={{padding: 10}}>
-    <TextInput
-      style={{height: 40}}
-      placeholder="Type here to translate!"
-      onChangeText={newText => setText(newText)}
-      defaultValue={text}
-    />
-    <Text style={{padding: 10, fontSize: 42}}>
-      {text
-        .split(' ')
-        .map(word => word && '🍕')
-        .join(' ')}
-    </Text>
-  </View>
-
-
-    
-    
-    // <View style={styles.container}>
-    //   <Text style={ {fontSize: 25}}>
-    //     Welcome to the Pasture Manager!
-    //   </Text>
+   
+<>    
+    <View style={styles.container}>
+      <Text style={ {fontSize: 25}}>
+        Welcome to the Pasture Manager!
+      </Text>
       
-    //   <TextInput
-    //     style={{height: 50}}
+      <TextInput
+        style={{height: 50}}
         
-    //     placeholder="Enter Field Name"
-    //     onChangeText={newPastureName => setpastureName(newPastureName)}
-    //     defaultValue={pastureName}
-    //   />
-    //    <Text style={{padding: 10, fontSize: 15}}>
+        placeholder="Enter Field Name"
+        onChangeText={newPastureName => setpastureName(newPastureName)}
+        defaultValue={pastureName}
+      />
+
+
+        <TextInput
+        style={{height: 50}}
+        
+        placeholder="Enter Field lbs/in"
+        onChangeText={newFieldDensity => setText(newFieldDensity)}
+        defaultValue={fieldDensity}
+      />
+
+      
+      <TextInput
+        style={{height: 50}}
+        
+        placeholder="Enter Field height"
+        onChangeText={newPastureHeight => setpastureHeight(newPastureHeight)}
+        defaultValue={pastureHeight}
+      />
        
 
-
-    //     The new field is {pastureName}
-    //     </Text> */
-
-
-    //    /* <TextInput
-    //     style={{height: 50}}
+      <TextInput
+        style={{height: 50}}
         
-    //     placeholder="Enter Field lbs/in"
-    //     onChangeText={newFieldDensity => setText(newFieldDensity)}
-    //     defaultValue={fieldDensity}
-    //   />
-    //    <Text style={{padding: 10, fontSize: 15}}>
-    //    </Text>
+        placeholder="Enter number of Horses"
+        onChangeText={newHorseCount=> sethorseCount(newHorseCount)}
+        defaultValue={horseCount}
+      />
 
 
+      <Text style={{padding: 10, fontSize: 15}}>
+        The new field is {pastureName}.
+        The number of days you can graze this pasture is {feedDays}.
+        </Text> 
 
-    //   <TextInput
-    //     style={{height: 50}}
-        
-    //     placeholder="Enter Field height"
-    //     onChangeText={newPastureHeight => setpastureHeight(newPastureHeight)}
-    //     defaultValue={pastureHeight}
-    //   />
-    //    <Text style={{padding: 10, fontSize: 15}}>
-    //     </Text>
-
-
-    //   <TextInput
-    //     style={{height: 50}}
-        
-    //     placeholder="Enter number of Horses"
-    //     onChangeText={newHorseCount=> sethorseCount(newHorseCount)}
-    //     defaultValue={horseCount}
-    //   />
-    //   </Text> */
-
-      
-
-
-      // <StatusBar style="auto" />
+        <Text style={{padding: 10, fontSize: 15}}>
+        The {grassMix} pasture is providing {tonsDryFeed} pounds of hay. 
+        </Text> 
+       
     
 
+      <StatusBar style="auto" />
 
+      </View>
+</>
 
       );
 }
